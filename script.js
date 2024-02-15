31f94a37cb4996edeef9092613982e8f16e22c8e
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2024-02-15 23:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+//your JS code here. If required
+document.addEventListener("DOMContentLoaded", () => {
+	const btn = document.getElementById("btn");
+
+	btn.addEventListener("click", async () => {
+		const textInput = document.getElementById("text");
+		const delayInput = (document.getElementById("delay");
+
+		if(textInput === "" || isNaN(delayNumber)){
+			alert("Please enter a valid code");
+			return;
+		}
+
+		try{
+			await delay(delayInput);
+			const outputDiv = document.getElementById("output");
+			outputDiv.textContent = textInput;
+			
+		}
+		catch(error){
+			console.error(error);
+		}
+		
+	});
+});
